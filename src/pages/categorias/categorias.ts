936dadd3e0884e778d04712b916099e0ebf47195
samaratg <samara11_09@hotmail.com>
2021-03-04 23:15:03 -0300

    Mostrando as categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categorias.dto';
 import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
@@ -16,12 +18,17 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(response => {
-      console.log(response);
+    this.categoriaService.findAll()
+    .subscribe(response => {
+      this.items = response;
     },
     error => {
       console.log(error);
